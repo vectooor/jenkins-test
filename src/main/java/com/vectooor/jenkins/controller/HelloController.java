f7ee2b1f3fe94dff2524ad27dd27185ad6c4429a
欧阳剑超 <>
2020-08-22 01:14:51 +0800

    init

---
+++ src/main/java/com/vectooor/jenkins/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.vectooor.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @author vector
+ * @date 2020/8/22 上午1:01
+ */
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping("world")
+    public String world() {
+        return "hello, world! Date=" + new Date();
+    }
+}
